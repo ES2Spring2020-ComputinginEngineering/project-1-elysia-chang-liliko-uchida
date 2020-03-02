2362ff68b3d6a13299aab72948b481e2484fe98e
lilikouchida930 <Ririko.Uchida@tufts.edu>
2020-03-02 15:14:01 -0500

    Update receiver.py

--- Step 3/receiver.py
+++ Step 3/receiver.py
@@ -7,7 +7,7 @@ import radio  # Needs to be imported separately
 
 # Change the channel if other microbits are interfering. (Default=7)
 radio.on()  # Turn on radio
-radio.config(channel=7, length =100)
+radio.config(channel=15, length =100)
 
 print('Program Started')
 mb.display.show(mb.Image.HAPPY, delay=1000, clear=True)
@@ -25,11 +25,17 @@ while True:
 
     if incoming is not None: # message was received
         mb.display.show(mb.Image.HEART, delay=100, clear=True, wait=False)
-        
+
         #############################################################
         # FILL IN HERE
-        # Incoming is string sent from logger
+
+    ######################################################
+
+        incoming = radio.receive()
+        print(incoming)
+        fout = open("incoming.txt", "w")
+        fout.write(incoming)
         # Need to parse it and reformat as a tuple for the MU plotter
         #############################################################
 
-        mb.sleep(10)
+        mb.sleep(100)
