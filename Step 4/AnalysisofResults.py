6ce9bf9bd6d754aeb4b410cb466693f6613a2fd6
elysiachang <60266730+elysiachang@users.noreply.github.com>
2020-03-04 20:52:47 -0500

    commit

--- Step 4/AnalysisofResults.py
+++ Step 4/AnalysisofResults.py
@@ -41,6 +41,7 @@ import csv
 import os
 import numpy as np
 import matplotlib.pyplot as plt
+import math
 
 path = "/Users/elysiachang/Documents/GitHub/project-1-elysia-chang-liliko-uchida/Step 3"
 
@@ -53,6 +54,7 @@ third_trial = 'sixteen.csv'
 fourth_trial = 'eighteen.csv'
 fifth_trial = 'twenty.csv'
 
+# FIRST TRIAL
 def file(first_trial):
     with open(first_trial) as csvfile:
         reader = csv.reader(csvfile, delimiter=',')
@@ -61,16 +63,39 @@ def file(first_trial):
             first_lengthsArray = np.array(length)
         return first_lengthsArray # last piece of data
 
+file(first_trial)
+first_lengthsArray = file(first_trial)
+
+#def find_tilt_y(first_lengthsArray):
+#    y = first_lengthsArray[1]
+#    x = math.sqrt(first_lengthsArray[0]**2 + first_lengthsArray[2]**2)
+#    tilt_y = math.atan2(y, x)
+#    return tilt_y
+#
+#def find_tilt_z(first_lengthsArray):
+#    y = math.sqrt(first_lengthsArray[0]**2 + first_lengthsArray[1]**2)
+#    x = first_lengthsArray[2]
+#    tilt_z = math.atan2(y, x)
+#    return tilt_z
+
+#def new_array(first_lengthsArray):
+    #time = first_lengthsArray[3]
+#    y = first_lengthsArray[:,0]
+#    x = math.sqrt((first_lengthsArray[1])**2 + (first_lengthsArray[2])**2)
+#    tilt_x = math.atan2(y, x)
+#    return tilt_x
+#
+#new_array(first_lengthsArray)
+
 #file(first_trial) # call the function
-first_lengthsArray = file(first_trial) # properly return variables into functions
+#first_lengthsArray = file(first_trial) # properly return variables into functions
 
-accx_first_trial = first_lengthsArray[:,0]
-accx_first_trial_array = np.array(accx_first_trial)
-time_first_trial = first_lengthsArray[:,3]
-time_first_trial_array = np.array(time_first_trial)
-
-print(time_first_trial_array)
-
+#accx_first_trial = first_lengthsArray[:,0]
+#accx_first_trial_array = np.array(accx_first_trial)
+#time_first_trial = first_lengthsArray[:,3]
+#time_first_trial_array = np.array(time_first_trial)
+#
+#
 #plt.plot(accx_first_trial_array, time_first_trial_array)
 
 #def new_array(trial):
@@ -84,12 +109,56 @@ print(time_first_trial_array)
 #    x = math.sqrt(acc_y**2 + acc_z**2)
 #    tilt_x = math.atan2(y, x)
 
-#fin = open("twelve.csv", "r", encoding="utf8")
+
+
+# SECOND TRIAL
+def file(second_trial):
+    with open(second_trial) as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+        for row in reader: # this changes all to a list - now, change it to an array
+            length = row
+            second_lengthsArray = np.array(length)
+        return second_lengthsArray # last piece of data
+
+file(second_trial)
+second_lengthsArray = file(second_trial)
+
+
+# THIRD TRIAL
+def file(third_trial):
+    with open(third_trial) as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+        for row in reader: # this changes all to a list - now, change it to an array
+            length = row
+            third_lengthsArray = np.array(length)
+        return third_lengthsArray # last piece of data
+
+file(third_trial)
+third_lengthsArray = file(third_trial)
+
+
+# FOURTH TRIAL
+def file(fourth_trial):
+    with open(fourth_trial) as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+        for row in reader: # this changes all to a list - now, change it to an array
+            length = row
+            fourth_lengthsArray = np.array(length)
+        return fourth_lengthsArray # last piece of data
+
+file(fourth_trial)
+fourth_lengthsArray = file(fourth_trial)
+
+
+
+# FIFTH TRIAL
+#def file(first_trial):
+#    with open(first_trial) as csvfile:
+#        reader = csv.reader(csvfile, delimiter=',')
+#        for row in reader: # this changes all to a list - now, change it to an array
+#            length = row
+#            first_lengthsArray = np.array(length)
+#        return first_lengthsArray # last piece of data
 #
-#def readcsv(fin):
-#    data = read.csv(fin)
-#    return(np.array(data))
-#    
-#readcsv(fin)
-#
-#fin.close()
+#file(first_trial)
+#first_lengthsArray = file(first_trial)
